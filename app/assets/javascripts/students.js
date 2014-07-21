8c402bf2820e3033a134f55ed06b3ce5d1d68185
Adrian Prisacaru <adi_prisacaru@yahoo.com>
2014-07-21 13:43:40 +0300

    improved datepicker

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,7 +1,10 @@
 $(document).on('ready page:load', function () {
   $(function() {
     $('.datepicker').datepicker({
-      dateFormat: "yy-mm-dd"
+      dateFormat: "yy-mm-dd",
+      changeMonth: true,
+      changeYear: true,
+      yearRange: "-70:-0"
     });
   });
 });
